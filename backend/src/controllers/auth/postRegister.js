2a24256f0a83b42434a599fa9e384b3422b9bfa6
Romit Tiwari <romittiwari100@gmail.com>
2024-08-28 17:39:40 +0530

    removing empty lines

--- backend/src/controllers/auth/postRegister.js
+++ backend/src/controllers/auth/postRegister.js
@@ -6,40 +6,28 @@ import jwt from "jsonwebtoken";
 export const postRegister = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-
     const userExists = await User.exists({ email });
-
     if (userExists) {
       return res.status(409).send("E-mail already in use");
     }
-
     const encryptedPassword = await bcrypt.hash(password, 10);
-
     const newChannel = await Channel.create({});
-
     const user = await User.create({
       username,
       email: email.toLowerCase(),
       password: encryptedPassword,
       channel: newChannel._id,
     });
-
-    // create JWT token
     const token = jwt.sign(
-      // user details which we would like to encrypt in JWT token
       {
         userId: user._id,
         email: user.email,
       },
-      // secret
       process.env.TOKEN_KEY,
-      // addtional config
       {
         expiresIn: "8h",
       }
     );
-
-    // send success response back to the user with data of register user and JWT
     return res.status(201).json({
       userDetails: {
         email: user.email,
--- backend/src/models/Channel.js
+++ backend/src/models/Channel.js
@@ -1,11 +1,8 @@
 import mongoose from "mongoose";
 import { v4 as uuid } from "uuid";
-
 const { Schema } = mongoose;
-
 const defaultTitle = "New Channel";
 const defaultDescription = "This is new channel description";
-
 const channelSchema = new Schema({
   isActive: { type: Boolean, default: false },
   title: { type: String, default: defaultTitle },
